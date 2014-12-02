562fbade5c39f8a4fc391e37dfc797850a1ec581
Ted Bowman <ted.git@tedbow.com>
2014-12-02 11:40:13 -0500

    Updated deploy script to revert features

--- hooks/dev/post-code-deploy/post-code-deploy.sh
+++ hooks/dev/post-code-deploy/post-code-deploy.sh
@@ -2,12 +2,12 @@
 #
 # Cloud Hook: post-code-deploy
 #
-# The post-code-deploy hook is run whenever you use the Workflow page to 
+# The post-code-deploy hook is run whenever you use the Workflow page to
 # deploy new code to an environment, either via drag-drop or by selecting
-# an existing branch or tag from the Code drop-down list. See 
+# an existing branch or tag from the Code drop-down list. See
 # ../README.md for details.
 #
-# Usage: post-code-deploy site target-env source-branch deployed-tag repo-url 
+# Usage: post-code-deploy site target-env source-branch deployed-tag repo-url
 #                         repo-type
 
 site="$1"
@@ -22,3 +22,7 @@ if [ "$source_branch" != "$deployed_tag" ]; then
 else
     echo "$site.$target_env: Deployed $deployed_tag."
 fi
+
+drush updatedb -y
+
+drush fr-all -y
